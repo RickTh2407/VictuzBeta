40034713999ffddfb830ed1ef18feeae66801caf
RickTh <rick.thewissen@gmail.com>
2024-12-05 16:14:27 +0100

    connectionstring setup, see Appsettings.json under the main project

--- VictuzBeta/obj/Debug/net8.0/VictuzBeta.AssemblyInfo.cs
+++ VictuzBeta/obj/Debug/net8.0/VictuzBeta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VictuzBeta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8110a429e63d26f83691da613af703b4a8047eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8194a4f5f67c99f48338d4fc4edc3163c74a9c8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VictuzBeta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VictuzBeta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
