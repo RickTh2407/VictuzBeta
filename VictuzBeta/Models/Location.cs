09cca854ff7b7e883d81c8f7d0af267405f9b428
SkiteV1 <msliekerwork@outlook.com>
2024-12-06 14:33:14 +0100

    ...

--- VictuzBeta/Models/Location.cs
+++ VictuzBeta/Models/Location.cs
@@ -7,20 +7,12 @@ namespace VictuzBeta.Models
         [Key]
         public int Id { get; set; }
         [Required]
-<<<<<<< HEAD
         public string? Name { get; set; }
         [Required]
         public string? Address { get; set; }
         [Required]
         public string? Room { get; set; }
         public virtual ICollection<Activity>? Activities { get; set; }
-=======
-        public string Name { get; set; }
-        [Required]
-        public string Address { get; set; }
-        [Required]
-        public string Room { get; set; }
->>>>>>> 40034713999ffddfb830ed1ef18feeae66801caf
 
     }
 }
