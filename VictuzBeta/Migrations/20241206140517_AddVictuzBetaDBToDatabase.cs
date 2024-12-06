a57b98f0c0f7a31332cc8cc9f3a1b4c6365e2e8b
SkiteV1 <msliekerwork@outlook.com>
2024-12-06 17:04:08 +0100

    smaal update 2.0

---
+++ VictuzBeta/Migrations/20241206140517_AddVictuzBetaDBToDatabase.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VictuzBeta.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVictuzBetaDBToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agendas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Room = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Newses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Newses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ScreenName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Validated = table.Column<bool>(type: "bit", nullable: false),
+                    Board = table.Column<bool>(type: "bit", nullable: false),
+                    NewsId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Members_Newses_NewsId",
+                        column: x => x.NewsId,
+                        principalTable: "Newses",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemberId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_Members_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Members",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Propositions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MemberName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusDisplay = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusesId = table.Column<int>(type: "int", nullable: true),
+                    MembersId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Propositions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Propositions_Members_MembersId",
+                        column: x => x.MembersId,
+                        principalTable: "Members",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Propositions_Statuses_StatusesId",
+                        column: x => x.StatusesId,
+                        principalTable: "Statuses",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LocationDisplay = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AgendasId = table.Column<int>(type: "int", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Activities_Agendas_AgendasId",
+                        column: x => x.AgendasId,
+                        principalTable: "Agendas",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Activities_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActivityLocation",
+                columns: table => new
+                {
+                    ActivitiesId = table.Column<int>(type: "int", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityLocation", x => new { x.ActivitiesId, x.LocationId });
+                    table.ForeignKey(
+                        name: "FK_ActivityLocation_Activities_ActivitiesId",
+                        column: x => x.ActivitiesId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActivityLocation_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActivityMember",
+                columns: table => new
+                {
+                    ActivitiesId = table.Column<int>(type: "int", nullable: false),
+                    MembersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityMember", x => new { x.ActivitiesId, x.MembersId });
+                    table.ForeignKey(
+                        name: "FK_ActivityMember_Activities_ActivitiesId",
+                        column: x => x.ActivitiesId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActivityMember_Members_MembersId",
+                        column: x => x.MembersId,
+                        principalTable: "Members",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Activities",
+                columns: new[] { "Id", "AgendasId", "Category", "CategoryId", "Date", "Description", "LocationDisplay", "Name" },
+                values: new object[] { 1, null, "Workshop", null, new DateTime(2024, 12, 6, 15, 5, 16, 463, DateTimeKind.Local).AddTicks(2192), "Test Description", "Test room", "Test Name" });
+
+            migrationBuilder.InsertData(
+                table: "Agendas",
+                columns: new[] { "Id", "Date", "Name" },
+                values: new object[] { 1, new DateTime(2024, 12, 6, 15, 5, 16, 465, DateTimeKind.Local).AddTicks(502), "Test" });
+
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "Id", "Address", "Name", "Room" },
+                values: new object[] { 1, "Test adress", "Test Name", "B3.210" });
+
+            migrationBuilder.InsertData(
+                table: "Members",
+                columns: new[] { "Id", "Board", "Email", "LastName", "Name", "NewsId", "Password", "ScreenName", "Validated" },
+                values: new object[] { 1, true, "administrator@gmail.com", "Istrator", "Admin", null, "admin", "Tester", true });
+
+            migrationBuilder.InsertData(
+                table: "Newses",
+                columns: new[] { "Id", "CreatedDate", "Description", "Title" },
+                values: new object[] { 1, new DateTime(2024, 12, 6, 15, 5, 16, 465, DateTimeKind.Local).AddTicks(3846), "Test description", "Test Title" });
+
+            migrationBuilder.InsertData(
+                table: "Propositions",
+                columns: new[] { "Id", "Date", "Description", "MemberName", "MembersId", "Name", "StatusDisplay", "StatusesId" },
+                values: new object[] { 1, new DateTime(2024, 12, 6, 15, 5, 16, 465, DateTimeKind.Local).AddTicks(2730), "Test", "Test", null, "Test", "In behandeling", null });
+
+            migrationBuilder.InsertData(
+                table: "Statuses",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Test", "In Progress" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_AgendasId",
+                table: "Activities",
+                column: "AgendasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_CategoryId",
+                table: "Activities",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityLocation_LocationId",
+                table: "ActivityLocation",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityMember_MembersId",
+                table: "ActivityMember",
+                column: "MembersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_MemberId",
+                table: "Categories",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_NewsId",
+                table: "Members",
+                column: "NewsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Propositions_MembersId",
+                table: "Propositions",
+                column: "MembersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Propositions_StatusesId",
+                table: "Propositions",
+                column: "StatusesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityLocation");
+
+            migrationBuilder.DropTable(
+                name: "ActivityMember");
+
+            migrationBuilder.DropTable(
+                name: "Propositions");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropTable(
+                name: "Agendas");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Newses");
+        }
+    }
+}
